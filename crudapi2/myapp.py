3759c88ba5faec0fead39c72e2cfc5aa83abc1e5
anuragsinghbuddha <anuragbuddha123@gmail.com>
2023-02-07 03:58:25 +0530

    function based api view

--- crudapi2/myapp.py
+++ crudapi2/myapp.py
@@ -9,7 +9,8 @@ def get_data(id=None):
     if id is not None:
         data={'id':id} 
     json_data=json.dumps(data)
-    r=requests.get(url=URL ,data=json_data)
+    headers={'content-Type':'application/json'}
+    r=requests.get(url=URL ,headers=headers,data=json_data)
     data=r.json()
     print(data) 
 
@@ -18,14 +19,15 @@ def get_data(id=None):
 #post/create data
 def post_data():
     data={
-        'name':'krishna',
-        'roll':10,
-        'city':'Mathura',
+        'name':'bala',
+        'roll':2,
+        'city':'purnia',
         
     }
     #above data is in python object hence convert in json and hit to url
+    headers={'content-Type':'application/json'}
     json_data=json.dumps(data)
-    r=requests.post(url=URL ,data=json_data)
+    r=requests.post(url=URL ,headers=headers,data=json_data)
     data=r.json()
     print(data)
 
@@ -34,32 +36,36 @@ def post_data():
 #put/update data
 def update_data():
     data={
-        'id':5,
-        'name':'om',
-        'city':'varansi'
+        'id':3,
+        'roll':3,
+        'name':'lalit',
+        'city':'jaipur'
         
     }
     #above data is in python object hence convert in json and hit to url
     json_data=json.dumps(data)
-    r=requests.put(url=URL ,data=json_data)
+    headers={'content-Type':'application/json'}
+    r=requests.put(url=URL ,headers=headers,data=json_data)
     data=r.json()
     print(data)
 
-update_data()
+#update_data()
 
 #delete/delete data 
 
 def delete_data():
     data={
-        'id':4, #only need to id ,which we wants to delete  
+        'id':3, #only need to id ,which we wants to delete  
     }
     #above data is in python object hence convert in json and hit to url
     json_data=json.dumps(data)
-    r=requests.delete(url=URL ,data=json_data)
+    headers={'content-Type':'application/json'}
+
+    r=requests.delete(url=URL ,headers=headers,data=json_data)
     data=r.json()
     print(data)
 
-#delete_data()
+delete_data()
 
 
 
